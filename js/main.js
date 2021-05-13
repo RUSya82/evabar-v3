bd82554032aed4a4f781886f2da4dbaad6c38ecb
RuslanSalimov <mail-spam2012@yandex.ru>
2021-05-13 07:43:07 +0500

    control commit

--- js/main.js
+++ js/main.js
@@ -24,3 +24,39 @@ var mySwiperPortfolio = new Swiper('.portfolio__slider', {
 	},
 });
 // mySwiperPortfolio.lazy.load();
+var mySwiperReview = new Swiper('.reviews__slider', {
+	// navigation: {
+	// 	nextEl: '.swiper-button-prev',
+	// 	prevEl: '.swiper-button-next',
+	// },
+	slidesPerView: 3,
+	speed: 400,
+	loop: true,
+	spaceBetween: 70,
+	centeredSlides: true,
+	slideToClickedSlide: true,
+	// autoHeight: true,
+	// zoom: true,
+	// toggle: true,
+	breakpoints: {
+		320: {
+			slidesPerView: 1
+		},
+		375: {
+			slidesPerView: 1
+		},
+		768: {
+			slidesPerView: 2
+		},
+		1100: {
+			slidesPerView: 2
+		}
+	},
+});
+//swiper.changeDirection(direction, needUpdate)
+// window.addEventListener('resize', () => {
+// 	const windowWidth = document.documentElement.clientWidth;
+// 	if(windowWidth < 768){
+// 		mySwiperReview.changeDirection('vertical', false)
+// 	}
+// });
