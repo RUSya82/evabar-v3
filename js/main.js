533f3198c859e9526055a6d1ad47a969b7e2d85c
RuslanSalimov <mail-spam2012@yandex.ru>
2021-05-18 02:04:51 +0500

    fixed slider

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ var mySwiperPortfolio = new Swiper('.portfolio__slider', {
 		}
 	},
 });
-// mySwiperPortfolio.lazy.load();
+//mySwiperPortfolio.lazy.load();
 var mySwiperReview = new Swiper('.reviews__slider', {
 	// navigation: {
 	// 	nextEl: '.swiper-button-prev',
@@ -53,10 +53,74 @@ var mySwiperReview = new Swiper('.reviews__slider', {
 		}
 	},
 });
-//swiper.changeDirection(direction, needUpdate)
-// window.addEventListener('resize', () => {
-// 	const windowWidth = document.documentElement.clientWidth;
-// 	if(windowWidth < 768){
-// 		mySwiperReview.changeDirection('vertical', false)
-// 	}
-// });
+// mySwiperReview.changeDirection(direction, needUpdate)
+window.addEventListener('resize', () => {
+	const windowWidth = document.documentElement.clientWidth;
+	if(windowWidth < 768){
+		mySwiperReview.changeDirection('vertical', true)
+	}
+});
+const windowWidth = document.documentElement.clientWidth;
+//let mySwiperReview;
+// if(windowWidth > 768){
+// 	mySwiperReview = new Swiper('.reviews__slider', {
+// 		// navigation: {
+// 		// 	nextEl: '.swiper-button-prev',
+// 		// 	prevEl: '.swiper-button-next',
+// 		// },
+// 		slidesPerView: 3,
+// 		speed: 400,
+// 		loop: true,
+// 		spaceBetween: 70,
+// 		centeredSlides: true,
+// 		slideToClickedSlide: true,
+// 		// autoHeight: true,
+// 		// zoom: true,
+// 		toggle: true,
+// 		breakpoints: {
+// 			320: {
+// 				slidesPerView: 1
+// 			},
+// 			375: {
+// 				slidesPerView: 1
+// 			},
+// 			768: {
+// 				slidesPerView: 2
+// 			},
+// 			1100: {
+// 				slidesPerView: 3
+// 			}
+// 		},
+// 	});
+// } else  {
+// 	mySwiperReview = new Swiper('.reviews__slider', {
+// 		// navigation: {
+// 		// 	nextEl: '.swiper-button-prev',
+// 		// 	prevEl: '.swiper-button-next',
+// 		// },
+// 		slidesPerView: 3,
+// 		speed: 400,
+// 		loop: true,
+// 		spaceBetween: 70,
+// 		centeredSlides: true,
+// 		slideToClickedSlide: true,
+// 		// autoHeight: true,
+// 		// zoom: true,
+// 		direction: 'vertical',
+// 		toggle: true,
+// 		breakpoints: {
+// 			320: {
+// 				slidesPerView: 1
+// 			},
+// 			375: {
+// 				slidesPerView: 1
+// 			},
+// 			768: {
+// 				slidesPerView: 2
+// 			},
+// 			1100: {
+// 				slidesPerView: 3
+// 			}
+// 		},
+// 	});
+// }
